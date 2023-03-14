d73c55bbfd38b67859470a6dc183cfe160de53d9
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-13 22:32:26 -0500

    test tank after drive within range

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -8,10 +8,7 @@ namespace CarTests
     {
         //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitHub account)
         [TestMethod]
-        public void EmptyTest()
-        {
-            Assert.AreEqual(10, 10, .001);
-        }
+        public void EmptyTest() => Assert.AreEqual(10, 10, .001);
 
         Car test_car;
 
@@ -29,6 +26,12 @@ namespace CarTests
         }
 
         //TODO: gasTankLevel is accurate after driving within tank range
+        [TestMethod]
+        public void TestGasTankAfterDriveWithinRange()
+        {
+            test_car.Drive(50);
+            Assert.AreEqual(9, test_car.GasTankLevel, .001);
+        }
         //TODO: gasTankLevel is accurate after attempting to drive past tank range
         //TODO: can't have more gas than tank size, expect an exception
 
